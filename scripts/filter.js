54ad6454b350160a8c489c6f31751dd65e034a73
MedBul <55193557+MedBul@users.noreply.github.com>
2020-04-09 10:11:06 +0500

    new filter changed

--- scripts/filter.js
+++ scripts/filter.js
@@ -31,3 +31,90 @@
     });
 
 }());
+
+(function () {
+
+    const elem = document.querySelector('.about-product');
+    const iso = new Isotope( elem, {
+
+  itemSelector: '.about-product__item',
+  filter: ".description "
+});
+
+    const controlls = document.querySelectorAll(".filter__link");
+    const activeClass = "filter__item_active";
+
+    controlls.forEach(function(control) {
+        
+        control.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const filterName = control.getAttribute("data-filter");
+
+            controlls.forEach(function(link) {
+                link.closest(".filter__item").classList.remove(activeClass);
+            })
+
+            control.closest(".filter__item").classList.add(activeClass);
+
+            iso.arrange({
+                filter: `.${filterName}`
+            })
+        });
+        
+    });
+
+}());
+
+
+(function () {
+
+    const elem = document.querySelector('.col-left');
+    const iso = new Isotope( elem, {
+
+  itemSelector: '.t-shirt',
+  filter: ".black "
+});
+
+    const controlls = document.querySelectorAll(".form-checkbox__circular");
+    const activeClass = "form-checkbox__circular-active";
+
+    controlls.forEach(function(control) {
+        
+        control.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const filterName = control.getAttribute("data-filter");
+
+            controlls.forEach(function(link) {
+                link.closest(".form-checkbox__circular").classList.remove(activeClass);
+            })
+
+            control.closest(".form-checkbox__circular").classList.add(activeClass);
+
+            iso.arrange({
+                filter: `.${filterName}`
+            })
+        });
+        
+    });
+
+}());
+
+(function () {
+    const controlls = document.querySelectorAll(".size__link");
+    const activeClass = "size__item_active";
+
+    controlls.forEach(function(control){
+        control.addEventListener("click", function(e){
+            e.preventDefault();
+
+        controlls.forEach(function(link){
+            link.closest(".size__item").classList.remove(activeClass);
+        })
+
+        control.closest(".size__item").classList.add(activeClass);
+
+        })
+    });
+}());
